xmas7/truffle

Add fetchStorage integration to running debugger CLI

packages/core/lib/debug/interpreter.js
@@ -6,7 +6,7 @@ const util = require("util");
 
 const DebugUtils = require("@truffle/debug-utils");
 const selectors = require("@truffle/debugger").selectors;
-const { session, sourcemapping, stacktrace, trace, evm, controller } =
+const { session, data, sourcemapping, stacktrace, trace, evm, controller } =
   selectors;
 
 const analytics = require("../services/analytics");
@@ -363,6 +363,7 @@ class DebugInterpreter {
 
     let alreadyFinished = this.session.view(trace.finishedOrUnloaded);
     let loadFailed = false;
+    let storageRangeAtActivationFailed = false;
 
     // If not finished, perform commands that require state changes
     // (other than quitting or resetting)
@@ -489,18 +490,41 @@ class DebugInterpreter {
         this.printer.print("");
       }
     }
-    if (cmd === "t") {
+    if (cmd === "t" || cmd === "z" || cmd === "Z") {
       if (!this.fetchExternal) {
         if (!this.session.view(session.status.loaded)) {
           const txSpinner = new Spinner(
             "core:debug:interpreter:step",
             DebugUtils.formatTransactionStartMessage()
           );
+          let setStorageVisible;
+          switch (cmd) {
+            case "t":
+              setStorageVisible = this.session.view(
+                data.application.allStorageVisible
+              );
+              break;
+            case "z":
+              setStorageVisible = false;
+              break;
+            case "Z":
+              setStorageVisible = true;
+              break;
+          }
           try {
-            await this.session.load(cmdArgs);
+            if (setStorageVisible) {
+              try {
+                await this.session.load(cmdArgs, { fetchStorage: true });
+              } catch {
+                await this.session.load(cmdArgs);
+                storageRangeAtActivationFailed = true;
+              }
+            } else {
+              await this.session.load(cmdArgs);
+            }
             txSpinner.succeed();
             this.repl.setPrompt(DebugUtils.formatPrompt(this.network, cmdArgs));
-          } catch (_) {
+          } catch {
             txSpinner.fail();
             loadFailed = true;
           }
@@ -521,6 +545,7 @@ class DebugInterpreter {
       if (!this.fetchExternal) {
         if (this.session.view(session.status.loaded)) {
           await this.session.unload();
+          this.printer.resetPopulatedStorageAddresses();
           this.printer.print("Transaction unloaded.");
           this.repl.setPrompt(DebugUtils.formatPrompt(this.network));
         } else {
@@ -644,7 +669,7 @@ class DebugInterpreter {
 
         if (this.session.view(session.status.loaded)) {
           if (this.session.view(trace.steps).length > 0) {
-            this.printer.printInstruction(temporaryPrintouts);
+            await this.printer.printInstruction(temporaryPrintouts);
             this.printer.printFile();
             this.printer.printState();
           } else {
@@ -673,7 +698,7 @@ class DebugInterpreter {
         break;
       case ";":
         if (!this.session.view(trace.finishedOrUnloaded)) {
-          this.printer.printInstruction();
+          await this.printer.printInstruction();
           this.printer.printFile();
           this.printer.printState();
         }
@@ -708,7 +733,7 @@ class DebugInterpreter {
       case "Y":
         if (!this.session.view(trace.finishedOrUnloaded)) {
           if (!this.session.view(sourcemapping.current.source).source) {
-            this.printer.printInstruction();
+            await this.printer.printInstruction();
           }
           this.printer.printFile();
           this.printer.printState();
@@ -726,9 +751,14 @@ class DebugInterpreter {
         }
         break;
       case "t":
+      case "z":
+      case "Z":
         if (!loadFailed) {
           this.printer.printAddressesAffected();
           this.printer.warnIfNoSteps();
+          if (storageRangeAtActivationFailed) {
+            this.printer.warnStorageRangeAtNotSupported();
+          }
           this.printer.printFile();
           this.printer.printState();
         } else if (this.session.view(session.status.isError)) {
@@ -760,6 +790,8 @@ class DebugInterpreter {
       cmd !== "-" &&
       cmd !== "t" &&
       cmd !== "T" &&
+      cmd !== "z" &&
+      cmd !== "Z" &&
       cmd !== "g" &&
       cmd !== "G" &&
       cmd !== "s" &&

packages/core/lib/debug/printer.js
@@ -17,6 +17,7 @@ class DebugPrinter {
   constructor(config, session) {
     this.config = config;
     this.session = session;
+    this.storagePopulatedAddresses = new Set();
     this.select = expr => {
       let selector, result;
 
@@ -89,6 +90,13 @@ class DebugPrinter {
     this.warnIfNoSteps();
     this.printHelp();
     debug("Help printed");
+    if (
+      !this.config.noFetchStorage && //sorry
+      !this.session.view(data.application.storageVisibilitySupported)
+    ) {
+      //if -z wasn't used, but debug_storageRangeAt isn't supported, warn
+      this.warnStorageRangeAtNotSupported();
+    }
     this.printFile();
     debug("File printed");
     this.printState();
@@ -118,6 +126,15 @@ class DebugPrinter {
         )} this transaction has no trace steps. This may happen if you are attempting to debug a transaction sent to an externally-owned account, or if the node you are connecting to failed to produce a trace for some reason. Please check your configuration and try again.`
       );
     }
+  }
+
+  warnStorageRangeAtNotSupported() {
+    this.config.logger.log("");
+    this.config.logger.log(
+      `${colors.bold(
+        "Warning:"
+      )} Your client does not support the debug_storageRangeAt method.  Only storage touched in the transaction being debugged will be visible.`
+    );
   }
 
   printHelp(lastCommand) {
@@ -215,7 +232,8 @@ class DebugPrinter {
     this.config.logger.log("");
   }
 
-  printInstruction(locations = this.locationPrintouts) {
+  //this is async because it may need to populate storage
+  async printInstruction(locations = this.locationPrintouts) {
     const instruction = this.session.view(sourcemapping.current.instruction);
     const instructions = this.session.view(sourcemapping.current.instructions);
     const step = this.session.view(trace.step);
@@ -228,6 +246,7 @@ class DebugPrinter {
 
     this.config.logger.log("");
     if (locations.has("sto")) {
+      await this.populateStorage();
       this.config.logger.log(DebugUtils.formatStorage(storage));
       this.config.logger.log("");
     }
@@ -681,7 +700,7 @@ class DebugPrinter {
   }
 
   async printVariables(sectionOuts = this.sectionPrintouts) {
-    const values = await this.session.variables({ indicateUnknown: true });
+    const values = await this.getVariables();
     const sections = this.session.view(data.current.identifiers.sections);
 
     const sectionNames = {
@@ -742,7 +761,7 @@ class DebugPrinter {
    *        :!<trace.step.stack>[1]
    */
   async evalAndPrintExpression(raw, indent, suppress) {
-    let variables = await this.session.variables({ indicateUnknown: true });
+    let variables = await this.getVariables();
 
     //if we're just dealing with a single variable, handle that case
     //separately (so that we can do things in a better way for that
@@ -856,6 +875,35 @@ class DebugPrinter {
     });
     interpreter.run();
     return interpreter.pseudoToNative(interpreter.value);
+  }
+
+  async getVariables() {
+    const values = await this.session.variables({ indicateUnknown: true });
+    const fetchStorage = this.session.view(data.application.allStorageVisible);
+    if (fetchStorage) {
+      const address = this.session.view(evm.current.call).storageAddress;
+      if (address) {
+        this.storagePopulatedAddresses.add(address); //mark as populated
+      }
+    }
+    return values;
+  }
+
+  async populateStorage() {
+    const fetchStorage = this.session.view(data.application.allStorageVisible);
+    if (fetchStorage) {
+      const address = this.session.view(evm.current.call).storageAddress;
+      //don't populate for addresses that are already populated
+      if (address && !this.storagePopulatedAddresses.has(address)) {
+        await this.session.variables(); //throw away the result, we're just doing this
+        //for the side effect of populating storage
+        this.storagePopulatedAddresses.add(address); //mark as populated
+      }
+    }
+  }
+
+  resetPopulatedStorageAddresses() {
+    this.storagePopulatedAddresses.clear();
   }
 }
 

solidityFrance/testtruffle

packages/core/lib/debug/interpreter.js
@@ -363,6 +363,7 @@ class DebugInterpreter {
 
     let alreadyFinished = this.session.view(trace.finishedOrUnloaded);
     let loadFailed = false;
+    let storageRangeAtActivationFailed = false;
 
     // If not finished, perform commands that require state changes
     // (other than quitting or resetting)
@@ -489,18 +490,41 @@ class DebugInterpreter {
         this.printer.print("");
       }
     }
-    if (cmd === "t") {
+    if (cmd === "t" || cmd === "z" || cmd === "Z") {
       if (!this.fetchExternal) {
         if (!this.session.view(session.status.loaded)) {
           const txSpinner = new Spinner(
             "core:debug:interpreter:step",
             DebugUtils.formatTransactionStartMessage()
           );
+          let setStorageVisible;
+          switch (cmd) {
+            case "t":
+              setStorageVisible = this.session.view(
+                evm.application.storageLookup
+              );
+              break;
+            case "z":
+              setStorageVisible = false;
+              break;
+            case "Z":
+              setStorageVisible = true;
+              break;
+          }
           try {
-            await this.session.load(cmdArgs);
+            if (setStorageVisible) {
+              try {
+                await this.session.load(cmdArgs, { storageLookup: true });
+              } catch {
+                await this.session.load(cmdArgs);
+                storageRangeAtActivationFailed = true;
+              }
+            } else {
+              await this.session.load(cmdArgs);
+            }
             txSpinner.succeed();
             this.repl.setPrompt(DebugUtils.formatPrompt(this.network, cmdArgs));
-          } catch (_) {
+          } catch {
             txSpinner.fail();
             loadFailed = true;
           }
@@ -521,6 +545,7 @@ class DebugInterpreter {
       if (!this.fetchExternal) {
         if (this.session.view(session.status.loaded)) {
           await this.session.unload();
+          this.printer.resetPopulatedStorageAddresses();
           this.printer.print("Transaction unloaded.");
           this.repl.setPrompt(DebugUtils.formatPrompt(this.network));
         } else {
@@ -644,7 +669,7 @@ class DebugInterpreter {
 
         if (this.session.view(session.status.loaded)) {
           if (this.session.view(trace.steps).length > 0) {
-            this.printer.printInstruction(temporaryPrintouts);
+            await this.printer.printInstruction(temporaryPrintouts);
             this.printer.printFile();
             this.printer.printState();
           } else {
@@ -673,7 +698,7 @@ class DebugInterpreter {
         break;
       case ";":
         if (!this.session.view(trace.finishedOrUnloaded)) {
-          this.printer.printInstruction();
+          await this.printer.printInstruction();
           this.printer.printFile();
           this.printer.printState();
         }
@@ -708,7 +733,7 @@ class DebugInterpreter {
       case "Y":
         if (!this.session.view(trace.finishedOrUnloaded)) {
           if (!this.session.view(sourcemapping.current.source).source) {
-            this.printer.printInstruction();
+            await this.printer.printInstruction();
           }
           this.printer.printFile();
           this.printer.printState();
@@ -726,9 +751,14 @@ class DebugInterpreter {
         }
         break;
       case "t":
+      case "z":
+      case "Z":
         if (!loadFailed) {
           this.printer.printAddressesAffected();
           this.printer.warnIfNoSteps();
+          if (storageRangeAtActivationFailed) {
+            this.printer.warnStorageRangeAtNotSupported();
+          }
           this.printer.printFile();
           this.printer.printState();
         } else if (this.session.view(session.status.isError)) {
@@ -760,6 +790,8 @@ class DebugInterpreter {
       cmd !== "-" &&
       cmd !== "t" &&
       cmd !== "T" &&
+      cmd !== "z" &&
+      cmd !== "Z" &&
       cmd !== "g" &&
       cmd !== "G" &&
       cmd !== "s" &&

packages/core/lib/debug/printer.js
@@ -17,6 +17,7 @@ class DebugPrinter {
   constructor(config, session) {
     this.config = config;
     this.session = session;
+    this.storagePopulatedAddresses = new Set();
     this.select = expr => {
       let selector, result;
 
@@ -89,6 +90,13 @@ class DebugPrinter {
     this.warnIfNoSteps();
     this.printHelp();
     debug("Help printed");
+    if (
+      !this.config.noFetchStorage && //sorry
+      !this.session.view(evm.application.storageLookupSupported)
+    ) {
+      //if -z wasn't used, but debug_storageRangeAt isn't supported, warn
+      this.warnStorageRangeAtNotSupported();
+    }
     this.printFile();
     debug("File printed");
     this.printState();
@@ -118,6 +126,15 @@ class DebugPrinter {
         )} this transaction has no trace steps. This may happen if you are attempting to debug a transaction sent to an externally-owned account, or if the node you are connecting to failed to produce a trace for some reason. Please check your configuration and try again.`
       );
     }
+  }
+
+  warnStorageRangeAtNotSupported() {
+    this.config.logger.log("");
+    this.config.logger.log(
+      `${colors.bold(
+        "Warning:"
+      )} Your client does not support the debug_storageRangeAt method.  Only storage touched in the transaction being debugged will be visible.`
+    );
   }
 
   printHelp(lastCommand) {
@@ -215,7 +232,8 @@ class DebugPrinter {
     this.config.logger.log("");
   }
 
-  printInstruction(locations = this.locationPrintouts) {
+  //this is async because it may need to populate storage
+  async printInstruction(locations = this.locationPrintouts) {
     const instruction = this.session.view(sourcemapping.current.instruction);
     const instructions = this.session.view(sourcemapping.current.instructions);
     const step = this.session.view(trace.step);
@@ -228,6 +246,7 @@ class DebugPrinter {
 
     this.config.logger.log("");
     if (locations.has("sto")) {
+      await this.populateStorage();
       this.config.logger.log(DebugUtils.formatStorage(storage));
       this.config.logger.log("");
     }
@@ -681,7 +700,7 @@ class DebugPrinter {
   }
 
   async printVariables(sectionOuts = this.sectionPrintouts) {
-    const values = await this.session.variables({ indicateUnknown: true });
+    const values = await this.getVariables();
     const sections = this.session.view(data.current.identifiers.sections);
 
     const sectionNames = {
@@ -742,7 +761,7 @@ class DebugPrinter {
    *        :!<trace.step.stack>[1]
    */
   async evalAndPrintExpression(raw, indent, suppress) {
-    let variables = await this.session.variables({ indicateUnknown: true });
+    let variables = await this.getVariables();
 
     //if we're just dealing with a single variable, handle that case
     //separately (so that we can do things in a better way for that
@@ -856,6 +875,35 @@ class DebugPrinter {
     });
     interpreter.run();
     return interpreter.pseudoToNative(interpreter.value);
+  }
+
+  async getVariables() {
+    const values = await this.session.variables();
+    const fetchStorage = this.session.view(evm.application.storageLookup);
+    if (fetchStorage) {
+      const address = this.session.view(evm.current.call).storageAddress;
+      if (address) {
+        this.storagePopulatedAddresses.add(address); //mark as populated
+      }
+    }
+    return values;
+  }
+
+  async populateStorage() {
+    const fetchStorage = this.session.view(evm.application.storageLookup);
+    if (fetchStorage) {
+      const address = this.session.view(evm.current.call).storageAddress;
+      //don't populate for addresses that are already populated
+      if (address && !this.storagePopulatedAddresses.has(address)) {
+        await this.session.variables(); //throw away the result, we're just doing this
+        //for the side effect of populating storage
+        this.storagePopulatedAddresses.add(address); //mark as populated
+      }
+    }
+  }
+
+  resetPopulatedStorageAddresses() {
+    this.storagePopulatedAddresses.clear();
   }
 }
 